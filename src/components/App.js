79e15334b9f8962e6a46cb22488708503e266b85
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-04 12:28:46 +0100

    [Update] Remove inline comments in code

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import '../style.css';
 import '../Normalize.css';
-// import Big from 'big-js';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
