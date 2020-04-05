24ef138b8da9cc79a4e12e2ad820f5a5dc4c26e9
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-05 16:37:37 +0100

    [Update] Add default props and proptype to display comp

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,16 @@ import '../Normalize.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-const App = () => (
-  <div className="main-container">
-    <h1>The Calculator</h1>
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <h1>The Calculator</h1>
+        <Display result={this.state} />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Display = () => <div className="display" />;
+const Display = props => {
+  const { result } = props;
+  return (
+    <div>
+      <div className="display">{result}</div>
+    </div>
+  );
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
+};
 
 export default Display;
