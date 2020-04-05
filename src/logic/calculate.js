bfd8829fa29ca3209d432c6e7e01b971eff9e0af
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-05 02:06:44 +0100

    [Feature] Create operator module

---
+++ src/logic/calculate.js
@@ -0,0 +1,13 @@
+import 
+
+export default (obj, buttonName) => {
+  const isNum = (testStr) => {
+    return /\d/.test(testStr)
+  }
+
+  if (isNum(buttonName)) {
+    if (obj.next) {
+
+    }
+  }
+}
---
+++ src/logic/operate.js
@@ -0,0 +1,23 @@
+import Big from 'big.js'
+
+const operator = (num1, num2, operand) => {
+  let x  = Big(num1)
+  let y = Big(num2)
+
+  if (operand === '+') {
+    return x.plus(y).toString()
+  } else if (operand ==='-') {
+    return x.minus(y).toString()
+  } else if (operand === '÷') {
+    return x.div(y).toString()
+  } else if(operand === '*') {
+    return x.mul(y).toString()
+  } else if(operand === '%') {
+    return x.mod(y).toString()
+  } else {
+    return operand + ' does not exist'
+  }
+}
+
+
+export default operator;
