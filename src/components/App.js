df1b89b2ceef2da16fc2b2d3c9236e4bf30204ac
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-05 16:12:36 +0100

    [Update] Update calculate function, App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,28 @@ import '../style.css';
 import '../Normalize.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '../logic/calculate'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: 'hello',
+      total: null,
+      next: null,
+      operation: null
     };
   }
 
   handleClick(i) {
-    this.setState({
-      input: `${this.input} ${i}`,
-    });
+    this.setState(calculate(this.state, i));
   }
 
   render() {
-    const { input } = this.state;
+    const { result } = this.state;
     return (
       <div className="main-container">
         <h1>The Calculator</h1>
-        <Display output={input} />
+        <Display output={result.total || result.next || '0'} />
         <ButtonPanel onClick={i => this.handleClick(i).bind(this)} />
       </div>
     );
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,19 +1,103 @@
-import operate from './operate'
+import operator from './operate'
 
-const calculate = (obj, buttonName) => {
+const calculate = (data, buttonName) => {
   const isNum = (testStr) => {
     return /\d/.test(testStr)
   }
 
-  // obj = {
-  //   total: null,
-  //   next: null,
-  //   operation: null
-  // }
-
   if (isNum(buttonName)) {
-    if (obj.next) {
-      
+    if (data.next == '0' && buttonName === '0') {
+      return {}
+    }
+
+    if (data.operation) {
+      if (data.next) {
+        return { next: data.next + buttonName }
+      } else {
+        return { next: buttonName };
+      }
+    }
+
+    if (data.next) {
+      return {
+        next: data.next + buttonName,
+        total: null,
+      }
+    }
+    return {
+      next: buttonName,
+      total: null,
     }
   }
+
+  if (data.operation) {
+    return {
+      total: operator(data.total, data.next, data.operation),
+      next: null,
+      operation: buttonName
+    }
+  }
+
+  if (!data.next) {
+    return { operation: buttonName };
+  }
+
+  if (buttonName === '.') {
+    if (data.next) {
+      if (data.next.indexOf('.') !== -1) {
+        return {}
+      }
+      return { next: data.next + '.'}
+    }
+    if (data.operation) {
+      return { next: '0.' }
+    }
+    if (data.total) {
+      if (data.total.indexOf('.') !== -1) {
+        return {}
+      } else {
+        return { total: '0.' };
+      }
+    }
+    return { total: '0.'}
+  }
+
+  if (buttonName === '=') {
+    if (data.next && data.operation) {
+      return {
+        total: operator(data.total, data.next, data.operation),
+        next: null,
+        operation: null
+      };
+    } else {
+      return {}
+    }
+  }
+
+  if (buttonName === '+/-') {
+    if (data.next) {
+      return { next: (-1 * String(+(dat.next))) }
+    } else if (data.total) {
+      return { total: (-1 * String(data.total))}
+    } else {
+      return {}
+    }
+  }
+
+  if (buttonName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  return {
+    total: data.next,
+    next: null,
+    operation: buttonName
+  }
+
 }
+
+export default calculate;
