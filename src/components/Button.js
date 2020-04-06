6f10ce0556f17be927b42f7eb578f07f1a565ceb
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-06 10:07:00 +0100

    [Fix] Correct linting error

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,13 +11,6 @@ const Button = props => {
 Button.propTypes = {
   classname: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  color: PropTypes.string,
-  wide: PropTypes.bool.isRequired,
-  click: PropTypes.func.isRequired,
-};
-
-Button.defaultProps = {
-  color: '#F5913E',
 };
 
 export default Button;
